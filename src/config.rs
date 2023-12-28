9778b1bd6d521ea631fdb1f33ebef02c55a5b086
Maiori44 <felice.dangelo2013@gmail.com>
2023-12-28 23:44:02 +0100

    wip base

---
+++ src/config.rs
@@ -0,0 +1,62 @@
+use std::{
+	path::PathBuf,
+	fs::File,
+	process,
+	io,
+	fs,
+};
+use clap::{
+	builder::{Styles, styling::{AnsiColor, Effects}},
+	crate_description,
+	crate_authors,
+	Parser,
+};
+use colored::Colorize;
+
+#[derive(Parser)]
+#[command(
+	version,
+	styles = Styles::plain()
+		.header(AnsiColor::Green.on_default().effects(Effects::BOLD))
+		.usage(AnsiColor::Green.on_default().effects(Effects::BOLD))
+		.literal(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+		.placeholder(AnsiColor::Cyan.on_default())
+		.error(AnsiColor::Red.on_default().effects(Effects::BOLD))
+		.valid(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+		.invalid(AnsiColor::Yellow.on_default().effects(Effects::BOLD)),
+	about = format!("{}\nMade by {}", crate_description!(), crate_authors!()),
+	long_about = None
+)]
+struct Cli {
+    /// Path to the configuration file
+    #[arg(
+		short,
+		long,
+		default_value = if let Ok(home) = std::env::var("HOME") {
+			home + "/.config/xz-backup/config.toml"
+		} else {
+			String::from("config.toml")
+		}
+	)]
+    config_path: PathBuf,
+
+	/// Don't print anything.
+	#[arg(short, long)]
+	quiet: bool,
+}
+
+pub fn init() -> io::Result<()> {
+	let cli = Cli::parse();
+	if cli.quiet {
+		unimplemented!();
+	}
+	if !cli.config_path.exists() {
+		println!("{} configuration file not found, generating default...", "notice:".cyan().bold());
+		if let Some(parent) = cli.config_path.parent() {
+			fs::create_dir_all(parent)?;
+		}
+		File::create(cli.config_path)?;
+		process::exit(-1);
+	}
+	std::process::exit(1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use colored::Colorize;
+use std::{
+	process,
+	io,
+};
+
+mod config;
+
+fn handle_error<T>(e: io::Error) -> T {
+	eprintln!("{} {}", "error:".red().bold(), e.to_string().to_ascii_lowercase());
+	process::exit(e.raw_os_error().unwrap_or(-2))
+}
+
 fn main() {
-	
+	config::init().unwrap_or_else(handle_error);
 }
