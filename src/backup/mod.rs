976c05b59808699eee826ee3593b7b355738127b
Maiori44 <felice.dangelo2013@gmail.com>
2023-12-30 12:52:33 +0100

    use config to make stream

--- src/backup/mod.rs
+++ src/backup/mod.rs
@@ -1,5 +1,5 @@
 use std::{io::{self, Read, Write}, fs::File};
-use xz2::read::XzEncoder;
+use xz2::{read::XzEncoder, stream::MtStreamBuilder};
 use crate::config::config;
 
 mod scanner;
@@ -8,7 +8,16 @@ mod tar;
 pub fn init() -> io::Result<()> {
 	let (reader, writer) = os_pipe::pipe()?;
 	let tar_thread = tar::spawn_thread(writer);
-	let mut compressor = XzEncoder::new(reader, 9); //TODO: apply config
+	let config = config!();
+	let mut compressor = XzEncoder::new_stream(
+		reader,
+		MtStreamBuilder::new()
+			.preset(config.level)
+			.threads(config.threads)
+			.block_size(config.block_size)
+			.encoder()
+			.map_err(|e| io::Error::other(e.to_string()))?
+	);
 	let mut output = File::options().read(true).write(true).create_new(true).open(config!(name))?;
 	let mut buf = [0; 10240];
 	loop {
